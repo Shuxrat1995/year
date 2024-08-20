b7cd9a480c26a9dc4dbb64fcc6dd7da418514773
Shuxrat <shuxratturaev96@gmail.com>
2024-08-20 10:33:33 +0500

    year

---
+++ main.js
@@ -0,0 +1,4 @@
+let one = prompt (' Введите свое имя ')
+let two = prompt ('Введите нынешний год')
+let three = prompt ('Введите свой год')
+console.log(one + ' , ' + ' Ваш возраст ' + (two - three));
